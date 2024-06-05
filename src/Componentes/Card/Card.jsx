08ab9d4af9380d9361443e15be1746de158534c3
DANTE VERDI GUTIERREZ <47573312@ort>
2024-06-05 11:12:37 -0300

    ta feo el css pero bueno no tenia tiempo :v

--- src/Componentes/Card/Card.jsx
+++ src/Componentes/Card/Card.jsx
@@ -4,7 +4,6 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 
-{/*card deberia tener los props de tooooda la data de cada usuario */}
 const Card = (props) =>{
 
     const [show, setShow] = useState(false);
@@ -13,12 +12,12 @@ const Card = (props) =>{
     const handleShow = () => setShow(true);
   
     return (
-      <>
+      <div class="card">
 
       {/*gracias por el modal bootstrap 😈*/}
 
         <h6>{props.nameCompleto}</h6>
-        <Button variant="primary" onClick={handleShow}>
+        <Button variant="primary"  onClick={handleShow}>
           mas informacion
         </Button>
           <img src={props.foto} alt="foto" />
@@ -27,16 +26,16 @@ const Card = (props) =>{
           <Modal.Header>
             <Modal.Title>{props.nameCompleto}</Modal.Title>
           </Modal.Header>
-          <Modal.Body><img src={props.fotoperogrande}></img>email: {props.email} <br/> username: {props.username} <br /> domicilio: {props.address} <br /> pais (en ingles) : {props.country} </Modal.Body>
+          <Modal.Body><img src={props.fotoperogrande} alt="foto de perfil"></img>email: {props.email} <br/> username: {props.username} <br /> domicilio: {props.address} <br /> pais (en ingles) : {props.country} </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
+            <Button variant="secondary"  onClick={handleClose}>
               cerrar
             </Button>
             
           </Modal.Footer>
         </Modal>
 
-      </>
+      </div>
     );
 }
 
